dc5acc187467662778d1e01ae226900801f2f3d3
cdpaiva <cdpaivaa@gmail.com>
2024-04-18 13:41:42 -0400

    Changes paths to match new data organization

--- scripts/plot_downsampled_rois.py
+++ scripts/plot_downsampled_rois.py
@@ -1,17 +1,26 @@
 import os
+from pathlib import Path
 import numpy as np
 
 from pasnascope.animations import custom_animation
 from pasnascope import feature_extraction
 
+data_dir = Path('./data')
+experiments = [f.stem for f in data_dir.iterdir() if f.is_dir()]
 
-img_dir = os.path.join(os.getcwd(), 'data', 'downsampled')
+print('Enter experiment name, based on index:')
+for i, file in enumerate(experiments):
+    print(f'[{i}] {file}')
+
+e = int(input())
+experiment = experiments[e]
+
+img_dir = os.path.join(os.getcwd(), 'data', experiment, 'downsampled')
 
 # All structural channel movies end with the suffix ch2
 structs = [f for f in sorted(os.listdir(img_dir)) if f.startswith('ds')]
 
 print('Select movie to display, based on index:')
-
 for i, file in enumerate(structs):
     file_name = file.split('-')[1]
     print(f'[{i}] {file_name}')
