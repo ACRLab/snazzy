be5c6489f1e827c4756045cb6305b07cc11e1729
cdpaiva <cdpaivaa@gmail.com>
2024-07-04 14:58:21 -0400

    Estimes time given developmental time

--- pasna_analysis/embryo.py
+++ pasna_analysis/embryo.py
@@ -19,7 +19,8 @@ class Embryo:
             self.vnc_length[:, 1], sigma=3)
         emb_size_csv = vnc_len_csv.parents[1].joinpath('full-length.csv')
         self.size = self.get_emb_size(emb_size_csv)
-        self.interpolator = None
+        self.dev_time_interpolator = None
+        self.time_interpolator = None
 
     def import_data(self, csv_path: Path) -> np.ndarray:
         return np.loadtxt(csv_path, delimiter=',', skiprows=1)
@@ -31,16 +32,29 @@ class Embryo:
     def get_DT_from_time(self, time: np.ndarray | float) -> np.ndarray | float:
         '''Returns the estimated (by linear interpolation) developmental time 
         for a time series.'''
-        if self.interpolator is None:
+        if self.dev_time_interpolator is None:
             dvt = self.developmental_time()
             dvt_timepoints = self.vnc_length[:, 0]
-            interpolator = interp1d(dvt_timepoints, dvt,
-                                    kind='linear', fill_value='extrapolate')
-            self.interpolator = interpolator
-        dt = self.interpolator(time)
+            dev_time_interp = interp1d(dvt_timepoints, dvt,
+                                       kind='linear', fill_value='extrapolate')
+            self.dev_time_interpolator = dev_time_interp
+        dt = self.dev_time_interpolator(time)
         if dt.size == 1:
             return dt.item()
         return dt
+
+    def get_time_from_DT(self, dev_time: np.ndarray | float) -> np.ndarray | float:
+        '''Returns the estimated (by linear interpolation) time given a developmental time sequence.'''
+        if self.time_interpolator is None:
+            dvt = self.developmental_time()
+            dvt_timepoints = self.vnc_length[:, 0]
+            time_interp = interp1d(dvt, dvt_timepoints,
+                                   kind='linear', fill_value='extrapolate')
+            self.time_interpolator = time_interp
+        time = self.time_interpolator(dev_time)
+        if time.size == 1:
+            return time.item()
+        return time
 
     def get_id(self) -> int:
         '''Returns the number that identifies an embryo.'''
