7ce960b2958ccf9e553ec1d3256e96c1cc41dc1a
cdpaiva <cdpaivaa@gmail.com>
2024-11-06 15:43:19 -0500

    Adds optional field `has_transients` to Experiment and ExperimentConfig

--- pasna_analysis/experiment_config.py
+++ pasna_analysis/experiment_config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Literal
 
 
 @dataclass
@@ -6,3 +7,5 @@ class ExperimentConfig():
     """Provides configuration to create an Experiment."""
     first_peak_threshold: int
     to_exclude: list[int]
+    dff_strategy: Literal['baseline', 'local_minima'] = 'baseline'
+    has_transients: bool = False
