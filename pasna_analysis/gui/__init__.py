b8b9ff5ef56772644cfea67f0f28c8b1b6739cbb
cdpaiva <cdpaivaa@gmail.com>
2025-02-07 13:51:27 -0500

    feat(gui): add Worker class to support loading on different thread

--- pasna_analysis/gui/__init__.py
+++ pasna_analysis/gui/__init__.py
@@ -1,7 +1,7 @@
 from .compare_plot_window import ComparePlotWindow
 from .image_window import ImageSequenceViewer, ImageWindow
 from .interactive_plot import InteractivePlotWidget
-from .model import Model
+from .model import Model, Worker
 from .plot_window import PlotWindow
 from .sidebar import RemovableSidebar, FixedSidebar
 from .sliders import LabeledSlider
