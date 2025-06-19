b28d6e6082acad481d352d6625701c87637cc095
cdpaiva <cdpaivaa@gmail.com>
2025-06-19 14:32:21 -0400

    tests: extract clean up code to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import os
+
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def clear_pd_params():
+    yield
+    for dir in Path("tests/assets/data").iterdir():
+        pd_params_path = dir.joinpath("peak_detection_params.json")
+        if pd_params_path.exists():
+            os.remove(pd_params_path)
