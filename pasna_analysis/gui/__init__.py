ee8546e810af0d29e021cdc7504dd8d1cde490c6
cdpaiva <cdpaivaa@gmail.com>
2025-06-26 12:33:29 -0400

    refactor: move worker to separate file

--- pasna_analysis/gui/__init__.py
+++ pasna_analysis/gui/__init__.py
@@ -1,3 +1,4 @@
+from .worker import Worker
 from .compare_plot_window import ComparePlotWindow
 from .exp_params_dialog import ExperimentParamsDialog
 from .graph_switcher import GraphSwitcher
@@ -5,7 +6,7 @@ from .image_window import ImageSequenceViewer, ImageWindow
 from .interactive_plot import InteractivePlotWidget
 from .json_viewer import JsonViewer
 from .peak_matcher import PeakMatcher
-from .model import Model, Worker
+from .model import Model
 from .plot_window import PlotWindow
 from .sidebar import RemovableSidebar, FixedSidebar
 from .sliders import LabeledSlider
