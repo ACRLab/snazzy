69ac89785c0e986d9798022dcd11f885137aeaeb
cdpaiva <cdpaivaa@gmail.com>
2025-07-09 16:19:04 -0400

    fix: avoid IndexError when thres index is last row

--- tests/test_trace_phases.py
+++ tests/test_trace_phases.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import pytest
 
 from pasna_analysis import TracePhases
@@ -12,6 +11,37 @@ def test_finds_right_index_in_dist_matrix():
     dist_matrix[k, :] += 1
 
     assert TracePhases.apply_threshold_to_matrix(dist_matrix, thres) == k - 1
+
+
+def test_finds_right_index_in_last_row():
+    dist_matrix = np.random.random((10, 10))
+    thres = 1
+    k = 9
+    dist_matrix[k, :] += 1
+
+    assert TracePhases.apply_threshold_to_matrix(dist_matrix, thres) == k - 1
+
+
+def test_when_all_dists_below_thres_return_last_index():
+    dist_matrix = np.random.random((10, 10))
+    thres = 2
+
+    actual_index = TracePhases.apply_threshold_to_matrix(dist_matrix, thres)
+    expected_index = len(dist_matrix) - 1
+
+    assert actual_index == expected_index
+
+
+def test_when_reversing_and_all_dists_below_thres_return_last_index():
+    dist_matrix = np.random.random((10, 10))
+    thres = 2
+
+    actual_index = TracePhases.apply_threshold_to_matrix(
+        dist_matrix, thres, reverse=True
+    )
+    expected_index = 9
+
+    assert actual_index == expected_index
 
 
 def test_finds_right_index_in_dist_matrix_reverse():
