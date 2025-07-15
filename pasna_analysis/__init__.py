274da980e8c44502e682c6214d849a1636f00e2e
cdpaiva <cdpaivaa@gmail.com>
2025-07-15 13:31:46 -0400

    refactor: extract baseline values to Enum

--- pasna_analysis/__init__.py
+++ pasna_analysis/__init__.py
@@ -1,7 +1,7 @@
 from .config import Config
 from .trace_phases import TracePhases
 from .data_loader import DataLoader
-from .trace import Trace
+from .trace import BaselineStrategies, Trace
 from .embryo import Embryo
 from .experiment import Experiment
 from .experiment_config import ExperimentConfig
