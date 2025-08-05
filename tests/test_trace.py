82505bc1a4630d783fbb98fb89c62587679f5e04
cdpaiva <cdpaivaa@gmail.com>
2025-08-05 16:57:44 -0400

    tests: test can create Trace for regular activity data and no peaks data

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -8,6 +8,10 @@ from pasna_analysis import BaselineStrategies, Config, Trace
 VALID_DIR = Path("./tests/assets/data/20250210")
 
 
+def read_from_csv(csv_path):
+    return np.loadtxt(csv_path, delimiter=",", skiprows=1)
+
+
 @pytest.fixture(scope="function")
 def config():
     return Config(VALID_DIR)
@@ -16,8 +20,49 @@ def config():
 @pytest.fixture
 def activity():
     csv_path = Path.joinpath(VALID_DIR, "activity", "emb1.csv")
-    data = np.loadtxt(csv_path, delimiter=",", skiprows=1)
-    return data
+    return read_from_csv(csv_path)
+
+
+def test_can_access_trace_props(config, activity):
+    trace = Trace("emb1", activity, config)
+    assert trace.peak_idxes is not None
+    assert trace.peak_times is not None
+    assert trace.peak_intervals is not None
+    assert trace.peak_amplitudes is not None
+    assert trace.peak_bounds_indices is not None
+    assert trace.peak_bounds_times is not None
+    assert trace.peak_durations is not None
+    assert trace.peak_rise_times is not None
+    assert trace.peak_decay_times is not None
+    assert trace.peak_aucs is not None
+    assert trace.rms is not None
+
+
+def test_can_process_trace_without_peaks(config):
+    csv_path = Path.joinpath(VALID_DIR, "activity", "emb22.csv")
+    activity = read_from_csv(csv_path)
+
+    trace = Trace("emb22", activity, config)
+    assert trace.peak_idxes is not None
+    assert trace.peak_times is not None
+    assert trace.peak_intervals is not None
+    assert trace.peak_amplitudes is not None
+    assert trace.peak_bounds_indices is not None
+    assert trace.peak_bounds_times is not None
+    assert trace.peak_durations is not None
+    assert trace.peak_rise_times is not None
+    assert trace.peak_decay_times is not None
+    assert trace.peak_aucs is not None
+    assert trace.rms is not None
+
+
+def test_can_create_dsna_trace_and_calculate_dsna(config, activity):
+    config.update_params({"exp_params": {"has_dsna": True}})
+    trace = Trace("emb1", activity, config)
+    assert trace
+
+    dsna_start = trace.get_dsna_start(freq=0.02)
+    assert dsna_start
 
 
 def test_baseline_with_average_n_values():
