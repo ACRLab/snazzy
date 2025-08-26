43a410252b074516a02553dd0b66e846af0e0ed5
cdpaiva <cdpaivaa@gmail.com>
2025-08-26 10:45:47 -0400

    refactor: update centerline methods

--- snazzy_processing/snazzy_processing/centerline.py
+++ snazzy_processing/snazzy_processing/centerline.py
@@ -3,16 +3,18 @@ import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import ndimage as ndi
-from skimage.filters import threshold_multiotsu
-from skimage.measure import label
-from skimage.morphology import remove_small_holes, binary_opening, disk
 from skimage.draw import line
 from skimage.feature import peak_local_max
+from skimage.filters import threshold_multiotsu
+from skimage.measure import label
+from skimage.morphology import binary_opening, disk, remove_small_holes
 from sklearn import linear_model
 
 
 def binarize(image):
     """Returns a binary image using a low threshold."""
+    if image.ndim != 2:
+        raise ValueError("Image can only have 2 dimensions.")
     thr = threshold_multiotsu(image)
     bin_img = image > thr[0]
 
@@ -27,9 +29,9 @@ def binarize(image):
 
 def get_DT_maxima(image, thres_rel=0.6, min_dist=5):
     """Calculates a distance transform and returns local maxima points."""
-    distance = ndi.distance_transform_cdt(image, metric="chessboard")
+    distance_transform = ndi.distance_transform_cdt(image, metric="chessboard")
     return peak_local_max(
-        distance,
+        distance_transform,
         footprint=np.ones((5, 5)),
         threshold_rel=thres_rel,
         min_distance=min_dist,
@@ -65,14 +67,49 @@ def apply_ransac(coords):
     return regressor
 
 
-def centerline_dist(image, verbose=False, pixel_width=1.62, thres_rel=0.6, min_dist=5):
+def centerline_mask(img_shape, predictor):
+    if len(img_shape) != 2:
+        raise ValueError("Image can only have 2 dimensions.")
+
+    rows, cols = img_shape
+    x_start = 0
+    x_end = cols - 1
+    y_start, y_end = [int(y) for y in predictor([[x_start], [x_end]])]
+
+    rr, cc = line(y_start, x_start, y_end, x_end)
+    # the RANSAC estimations might fall out of the image range
+    # make sure that the estimate is within the image dimensions
+    rr = np.clip(rr, 0, rows - 1)
+
+    mask = np.zeros(img_shape, dtype=np.bool_)
+    mask[rr, cc] = True
+
+    return mask
+
+
+def measure_length(masked_image, pixel_width):
+    if masked_image.ndim != 2:
+        raise ValueError("Image can only have 2 dimensions.")
+
+    nonzeros = np.argwhere(masked_image)
+    nonzeros = np.sort(nonzeros, axis=0)
+
+    line_start, line_end = nonzeros[0], nonzeros[-1]
+
+    distance = np.sqrt(np.sum((line_start - line_end) ** 2))
+
+    return distance * pixel_width
+
+
+def centerline_dist(bin_image, pixel_width=1.62, thres_rel=0.6, min_dist=5):
     """Returns the centerline length estimation based on EDT maxima points."""
-    coords = get_DT_maxima(image, thres_rel, min_dist)
+    if bin_image.ndim != 2:
+        raise ValueError("Centerline distance can only be calculated on a 2D image.")
+    coords = get_DT_maxima(bin_image, thres_rel, min_dist)
 
     if coords.shape[0] <= 2:
-        if verbose:
-            print(f"Found a single point of maxima, cannot apply RANSAC.")
-        return None
+        print(f"Found a single point of maxima, cannot apply RANSAC.")
+        return 0
 
     try:
         estimator = apply_ransac(coords)
@@ -81,69 +118,43 @@ def centerline_dist(image, verbose=False, pixel_width=1.62, thres_rel=0.6, min_d
         # in these cases, we return 0 to signal that no distance was found
         return 0
 
-    # points to draw the line mask
-    x1 = 0
-    x2 = image.shape[1] - 1
-    y1, y2 = [int(y) for y in estimator.predict([[x1], [x2]])]
+    mask = centerline_mask(bin_image.shape, estimator.predict)
+    bin_image[~mask] = 0
 
-    rr, cc = line(y1, x1, y2, x2)
-    # the RANSAC estimations might be out of the image range
-    rr = np.clip(rr, 0, image.shape[0] - 1)
+    distance = measure_length(bin_image, pixel_width)
 
-    # create and apply mask
-    inner = np.zeros_like(image)
-    inner[rr, cc] = 1
-    image[~inner] = 0
-
-    # first and last points where the line intersects the image
-    nonzeros = np.argwhere(image)
-    nonzeros = np.sort(nonzeros, axis=0)
-    vnc_start = nonzeros[0]
-    vnc_end = nonzeros[-1]
-    distance = np.sqrt(np.sum((vnc_end - vnc_start) ** 2))
-
-    return distance * pixel_width
+    return distance
 
 
 def view_centerline_dist(image, img_title="", thres_rel=0.6, min_dist=5):
     """Returns the centerline length estimation based on EDT maxima points."""
-    orig_image = image.copy()
-    image = binarize(image)
-    coords = get_DT_maxima(image, thres_rel=thres_rel, min_dist=min_dist)
+    binary_image = binarize(image)
+    coords = get_DT_maxima(binary_image, thres_rel, min_dist)
 
-    y = coords.T[0]
-    x = coords.T[1].reshape(-1, 1)
     if coords.shape[0] <= 2:
+        print(f"Cound not find enough DT points of maxima.")
         return None
 
-    estimator = apply_ransac(coords)
+    y = coords.T[0]
+    # sklearn expects x to have shape (n_samples, n_features), ie (N, 1)
+    x = coords.T[1].reshape(-1, 1)
+
+    try:
+        estimator = apply_ransac(coords)
+    except ValueError:
+        print(f"Cound not find centerline for the given DT points.")
+        return None
+
     inliers = estimator.inlier_mask_
     outliers = np.logical_not(inliers)
 
-    # points to draw the line mask
-    x1 = 0
-    x2 = image.shape[1] - 1
-    y1, y2 = [int(y) for y in estimator.predict([[x1], [x2]])]
-
-    rr, cc = line(y1, x1, y2, x2)
-
-    # the RANSAC estimations might be out of the image range
-    rr = np.clip(rr, 0, image.shape[0] - 1)
-
-    # create and apply mask
-    inner = np.zeros_like(image)
-    inner[rr, cc] = 1
-    image[~inner] = 0
-
-    # first and last points where the line intersects the image
-    nonzeros = np.argwhere(image)
-    nonzeros = np.sort(nonzeros, axis=0)
+    mask = centerline_mask(image.shape, estimator.predict)
 
     fig, ax = plt.subplots()
     ax.scatter(x[inliers], y[inliers], color="green")
     ax.scatter(x[outliers], y[outliers], color="red")
-    ax.imshow(orig_image)
-    ax.imshow(inner, alpha=0.3)
+    ax.imshow(image)
+    ax.imshow(mask, alpha=0.3)
     ax.set_title(img_title)
     ax.set_axis_off()
     fig.canvas.header_visible = False
