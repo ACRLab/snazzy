905b0ad20f32c86d2e9fc70d8a5a42b67cafb62d
cdpaiva <cdpaivaa@gmail.com>
2025-09-09 11:20:18 -0400

    refactor: move all graphs to compare_plot_window

--- snazzy_analysis/snazzy_analysis/gui/__init__.py
+++ snazzy_analysis/snazzy_analysis/gui/__init__.py
@@ -6,7 +6,6 @@ from .interactive_plot import InteractivePlotWidget
 from .json_viewer import JsonViewer
 from .peak_matcher import PeakMatcher
 from .model import GroupModel, Model
-from .plot_window import PlotWindow
 from .sidebar import RemovableSidebar, FixedSidebar
 from .sliders import LabeledSlider
 from .phase_boundaries_window import PhaseBoundariesWindow
