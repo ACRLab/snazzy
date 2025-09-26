22ed8a8bfbfe58228378b04b11c5e8951964d57d
cdpaiva <cdpaivaa@gmail.com>
2025-09-26 15:38:23 -0400

    docs: update docstrings and type hints

--- snazzy_processing/snazzy_processing/csv_handler.py
+++ snazzy_processing/snazzy_processing/csv_handler.py
@@ -1,13 +1,42 @@
+from pathlib import Path
+
 import numpy as np
 
 
-def read(csv_path, delimiter=",", skip_header=1, usecols=None):
+def read(csv_path: Path, delimiter=",", skip_header=1, usecols=None) -> np.ndarray:
+    """Read csv file as a numpy array.
+
+    Parameters:
+        csv_path (Path):
+            Path to csv file.
+        delimiter (str):
+            Delimiter used in the csv file. Defaults to ','.
+        skip_header (int):
+            Number of header lines, that will be skipped.
+        usecols: (list[int] | None):
+            List with column numbers to read.
+    """
     return np.genfromtxt(
         csv_path, delimiter=delimiter, skip_header=skip_header, usecols=usecols
     )
 
 
-def write_files(csv_paths, signals, header, fmt="%.2f"):
+def write_files(
+    csv_paths: list[Path], signals: np.ndarray, header: list[str], fmt="%.2f"
+):
+    """Write data as csv.
+
+    Parameters:
+        csv_paths (list[Path]):
+            List of csv paths for files that will be created.
+        signals (np.ndarray):
+            Array with data to be saved as csv.
+        header (list[str]):
+            Column names.
+        fmt (str):
+            Format string. Refer to `np.savetxt` docs for details.
+            Defaults to "%.2f".
+    """
     for csv_path, signal in zip(csv_paths, signals):
         if csv_path.exists():
             print(f"File {csv_path.stem} already exists. Skipping..")
@@ -15,7 +44,20 @@ def write_files(csv_paths, signals, header, fmt="%.2f"):
         write_file(csv_path, signal, header, fmt)
 
 
-def write_file(csv_path, signal, header, fmt="%.2f"):
+def write_file(csv_path: Path, signal: np.ndarray, header: list[str], fmt="%.2f"):
+    """Write a csv file.
+
+    Parameters:
+        csv_path (Path):
+            Path to save the csv file.
+        signal (np.ndarray):
+            Array with data to be saved as csv.
+        header (list[str]):
+            Column names.
+        fmt (str):
+            Format string. Refer to `np.savetxt` docs for details.
+            Defaults to "%.2f".
+    """
     header_line = ", ".join(header)
     np.savetxt(
         csv_path,
--- snazzy_processing/snazzy_processing/vnc_length.py
+++ snazzy_processing/snazzy_processing/vnc_length.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 
 from snazzy_processing import centerline, csv_handler
@@ -10,8 +12,28 @@ def measure_VNC_centerline(
     min_dist=5,
     outlier_thres=0.09,
     threshold_method="multiotsu",
-):
-    """Calculates the centerline distance for a 3D image."""
+) -> np.ndarray:
+    """Calculates the centerline distance for a 3D image.
+
+    As RANSAC might give the wrong measurement sometimes, points with relative
+    difference above `outlier_thres` are corrected by lin regression.
+
+    Parameters:
+        image (np.ndarray):
+            3D matrix representing an image.
+        pixel_width (float):
+            Physica size of a pixel.
+        thres_rel (float):
+            Threshold value used to calculate centerline points.
+        min_dist (float):
+            Minimum distance used to find local points in the distance transform.
+        outlier_thres (float):
+            Maximum VNC length relative change between consecutive points.
+
+    Returns:
+        vnc_lenghts (np.ndarray):
+            Array with VNC measurements.
+    """
     vnc_lengths = np.zeros(image.shape[0])
     for i, img in enumerate(image):
         bin_img = centerline.binarize(img, threshold_method=threshold_method)
@@ -33,8 +55,13 @@ def measure_VNC_centerline(
     return vnc_lengths
 
 
-def predict_next(previous):
-    """Estimates the next point by linear regression."""
+def predict_next(previous: np.ndarray) -> np.ndarray:
+    """Estimates the next point by linear regression.
+
+    Parameters:
+        previous (np.ndarray):
+            Array with previous points, use to predict the next point.
+    """
     y = np.array(previous)
     x = np.arange(y.shape[0])
     A = np.vstack([x, np.ones(len(x))]).T
@@ -42,14 +69,29 @@ def predict_next(previous):
     return m * len(x) + c
 
 
-def get_length_from_csv(file_path, columns=(1,)):
+def get_length_from_csv(file_path: Path, columns=(1,)):
     """Reads CSV data as a nparray.
 
     Expects the lengths to be in actual metric units, instead of pixels."""
     return csv_handler.read(file_path, usecols=columns)
 
 
-def get_output_data(length_data, downsampling, frame_interval):
+def add_timepoints(
+    length_data: np.ndarray, downsampling: int, frame_interval: int
+) -> np.ndarray:
+    """Add time information (in seconds) to VNC length data.
+
+    Parameters:
+        lendth_data (np.ndarray):
+            An array of VNC length measurments.
+        downsampled (int):
+            Step interval used when calculating the VNC length.
+        frame_interval (int):
+            The interval of acquistion of frames in seconds.
+
+    Returns:
+        A np.ndarray of shape (N, t, 2).
+    """
     t = length_data.size
     time = np.arange(t) * frame_interval * downsampling
 
@@ -57,10 +99,23 @@ def get_output_data(length_data, downsampling, frame_interval):
 
 
 def export_csv(ids, lengths, output_dir, downsampling, frame_interval=6):
+    """Write vnc lengths as csv.
+
+    Parameters:
+        ids (list[int]):
+            Embryo Ids used to name each csv file.
+            Must match the indices of the signals list.
+        lengths (list):
+            VNC length measurements
+        output_dir (Path):
+            Path to write csv files.
+        downsampling (int):
+            Step interval used when calculating the VNC length.
+        frame_interval (int):
+            The interval of acquistion of frames in seconds.
+    """
     csv_paths = [output_dir.joinpath(f"emb{id}.csv") for id in ids]
 
     for length_data, csv_path in zip(lengths, csv_paths):
-        data = get_output_data(length_data, downsampling, frame_interval)
+        data = add_timepoints(length_data, downsampling, frame_interval)
         csv_handler.write_file(csv_path, data, ["time", "length"])
-
-    return True
