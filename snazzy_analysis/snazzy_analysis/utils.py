ddf4c157cd2aaaa315e484eee4d70d0c643bfdad
cdpaiva <cdpaivaa@gmail.com>
2025-10-02 14:16:35 -0400

    feat(gui): display embryo options sorted alphabetically

--- snazzy_analysis/snazzy_analysis/utils.py
+++ snazzy_analysis/snazzy_analysis/utils.py
@@ -8,9 +8,22 @@ def split_in_bins(arr: np.ndarray, bins: int):
 
 
 def emb_id(emb: Path | str) -> int:
-    """Retuns the number that identifies a given embryos.
+    """Retun the number that identifies a given embryos.
 
     Assumes that embryos are always named as emb + id, e.g: `emb21`."""
     if isinstance(emb, Path):
         emb = emb.stem
     return int(emb[3:])
+
+
+def emb_id_from_filename(emb_path: str) -> int:
+    """Return the embryo id based on filename.
+
+    Assumes that files are named as embXX-chY.
+
+    Parameters:
+        emb_path (str):
+            Full path to embryo file, as a string.
+    """
+    emb_name = Path(emb_path).stem
+    return int(emb_name.split("-")[0][3:])
