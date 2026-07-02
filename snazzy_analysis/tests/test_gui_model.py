9c52a9f81c72f828e3d85279cda5e9191689ebcd
cdpaiva <cdpaivaa@gmail.com>
2026-07-02 10:46:43 -0400

    tests: avoid OOB errors when setting manual peak

--- snazzy_analysis/tests/test_gui_model.py
+++ snazzy_analysis/tests/test_gui_model.py
@@ -143,14 +143,14 @@ def test_ignore_manual_peaks_after_trim_index(model_single_dataset):
     curr_emb = model_single_dataset.selected_dataset.get_embryo("emb1")
     curr_trace = curr_emb.trace
 
-    manual_peak_idx = len(curr_trace.dff) - 1
+    manual_peak_idx = len(curr_trace.dff) - 10
     model_single_dataset.add_peak(manual_peak_idx, "emb1", curr_trace)
 
     curr_emb.trace.detect_peaks()
 
     updated_trim_idx = len(curr_trace.dff) // 2
     model_single_dataset.save_trim_idx(updated_trim_idx)
-
+    
     curr_emb.trace.detect_peaks()
 
     assert manual_peak_idx not in curr_trace.peak_idxes
